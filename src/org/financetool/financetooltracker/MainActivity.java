7754ed943ac0b4ce076103d90d292f546fe651ff
David Raffensperger <contact@davidraff.com>
2013-07-22 17:57:54 -0400

    Started the Google login feature

--- src/org/financetool/financetooltracker/MainActivity.java
+++ src/org/financetool/financetooltracker/MainActivity.java
@@ -1,13 +1,26 @@
 package org.financetool.financetooltracker;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.Message;
 import android.location.*;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
@@ -31,6 +44,14 @@ public class MainActivity extends Activity {
         	
         }
     });
+    
+    final Button loginButton = (Button) findViewById(R.id.loginButton);
+    loginButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {        	
+        	doLogin();
+        }
+    });
+    
     
     final Button showDataButton = (Button) findViewById(R.id.showDataButton);
     showDataButton.setOnClickListener(new View.OnClickListener() {
@@ -99,5 +120,155 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private void doLogin() {
+		AccountManager am = AccountManager.get(this);
+    	Bundle options = new Bundle();
+
+    	/*
+    	 * 
+    	 I need to pop something up so that the user can choose which google
+    	 account to use.
+    	 I need to use http://developer.android.com/reference/android/app/FragmentManager.html
+    	 and http://developer.android.com/reference/android/app/DialogFragment.html
+ protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DIALOG_ACCOUNTS:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(R.string.select_google_account);
+                final Account[] accounts = accountManager.getAccountsByType(GOOGLE_TYPE);
+                final int size = accounts.length;
+                String[] names = new String[size];
+                for (int i = 0; i < size; i++) {
+                    names[i] = accounts[i].name;
+                }
+                builder.setItems(names, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        onAccountSelected(accounts[which]);
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        finish();
+                    }
+                });
+                return builder.create();
+        }
+        return null;
+    }    	
+    	 */
+    	
+    	Account[] accounts = am.getAccountsByType("com.google");
+    	
+    	AccountManagerFuture<Bundle> future = 
+    	am.getAuthToken(
+    		accounts[0],                     // Account retrieved using getAccountsByType()
+    	    "Manage your tasks",            // Auth scope
+    	    options,                        // Authenticator-specific options
+    	    this,                           // Your activity
+    	    new OnTokenAcquired(),          // Callback called when a token is successfully acquired
+    	    null);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK) {
+			// The user updated their login info, so try the login again.
+			doLogin();
+		}
+	}
+	
+	private class OnTokenError implements Callback {
+	    public void run(AccountManagerFuture<Bundle> result) {
+	        // Get the result of the operation from the AccountManagerFuture.
+	        try {
+				Bundle bundle = result.getResult();
+			} catch (OperationCanceledException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (AuthenticatorException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	     
+	    }
+
+		@Override
+		public boolean handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	}
+	
+	private class OnTokenAcquired implements AccountManagerCallback<Bundle> {
+	    @Override
+	    public void run(AccountManagerFuture<Bundle> result) {
+	        // Get the result of the operation from the AccountManagerFuture.
+	        Bundle bundle = null;
+			try {
+				bundle = result.getResult();
+			} catch (OperationCanceledException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (AuthenticatorException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        			
+			
+	        Intent launch = (Intent) bundle.get(AccountManager.KEY_INTENT);
+	        if (launch != null) {
+	            startActivityForResult(launch, 0);
+	            return;
+	        }
+	    
+	        // The token is a named value in the bundle. The name of the value
+	        // is stored in the constant AccountManager.KEY_AUTHTOKEN.
+	        String token = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+	        
+	        String your_api_key = "";
+	        String your_client_secret = "";
+	        String your_client_id = "";
+	        
+	        URL url = null;
+			try {
+				url = new URL("https://www.googleapis.com/tasks/v1/users/@me/lists?key=" + your_api_key);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        HttpURLConnection conn = null;
+			try {
+				conn = (HttpURLConnection) url.openConnection();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	        
+			conn.addRequestProperty("client_id", your_client_id);
+	        conn.addRequestProperty("client_secret", your_client_secret);
+	        conn.setRequestProperty("Authorization", "OAuth " + token);
+	        int code = -1;
+			try {
+				code = conn.getResponseCode();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	        if (code == 401) {
+	        	// Token expired
+	        	AccountManager am = AccountManager.get(MainActivity.this);
+	        	am.invalidateAuthToken("com.google", token);
+	        }
+	        
+	    }
 	}
 }
